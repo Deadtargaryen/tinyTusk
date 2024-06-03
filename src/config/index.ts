77f2cd886e3c1f37908d0e8f4e0deff4f151e845
Deadtargaryen <mannyetukudoh@gmail.com>
2024-06-03 18:58:47 +0100

    nav files addition, forked from digitalHippo

---
+++ src/config/index.ts
@@ -0,0 +1,13 @@
+export const PRODUCT_CATEGORIES =[
+    {
+        label: "UI KITS",
+        value: "ui_kits" as const,
+        featured: [
+            {
+                name: "Editor Picks",
+                href: "#",
+                imagesrc: "/nav/ui-kits/mixed.jpg"
+            }
+        ]
+    }
+]
